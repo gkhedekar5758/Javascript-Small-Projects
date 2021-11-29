f5d610e51245de2f131c6c6d53e6b632b816d7df
Gaurang Khedekar <khedekar5758@gmail.com>
2021-11-29 12:35:43 +0530

    completed code to fetch from the API in quiz.

--- Quiz Application/QUIZ APP - V2/PreQuiz.js
+++ Quiz Application/QUIZ APP - V2/PreQuiz.js
@@ -6,9 +6,12 @@ function startGame(){
     let category=CATEGORYELEMENT.value;
     let difficult=DIFFICULTYELEMENT.value;
 
-    //console.log(number +" "+category+" "+difficult)
+    console.log(number +" "+category+" "+difficult)
 
-    let urlForNextPage=`Quiz.html?amount=${encodeURIComponent(number)}&category=${encodeURIComponent(category)}&difficulty=${encodeURIComponent(difficult)}`;
+    let urlForNextPage=`Quiz.html?amount=${encodeURIComponent(number)}`; //&category=${encodeURIComponent(category)}&difficulty=${encodeURIComponent(difficult)}`;
+    if(category !=="") urlForNextPage=urlForNextPage.concat(`&category=${encodeURIComponent(category)}`);
+    if(difficult !== "") urlForNextPage=urlForNextPage.concat(`&difficulty=${encodeURIComponent(difficult)}`);
+    console.log(urlForNextPage)
     //let urlForNextPage=`Quiz.html`
     document.location.href=urlForNextPage;
 }
